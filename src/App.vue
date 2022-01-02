403ee50908b17a91ba6f8be81b27204edfc27893
btbrad <24836323+btbrad@users.noreply.github.com>
2022-01-02 22:07:34 +0800

    columnList done

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import 'normalize.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import Logo from './assets/logo.png'
 const testData: ColumnProps[] = [
   {
     id: 1,
     title: 'test1的专栏',
     description: '这是test1的专栏，请点击查看!',
-    avatar: 'https://thumbs.dreamstime.com/z/male-avatar-icon-flat-style-male-user-icon-cartoon-man-avatar-hipster-vector-stock-91462914.jpg'
+    avatar: Logo
   },
   {
     id: 2,
     title: 'test2的专栏',
     description: '这是test2的专栏，请点击查看!',
-    avatar: 'https://thumbs.dreamstime.com/z/male-avatar-icon-flat-style-male-user-icon-cartoon-man-avatar-hipster-vector-stock-91462914.jpg'
+    avatar: Logo
+  },
+  {
+    id: 3,
+    title: 'test3的专栏',
+    description: '这是test3的专栏，请点击查看!',
+    avatar: Logo
+  },
+  {
+    id: 4,
+    title: 'test4的专栏',
+    description: '这是test4的专栏，请点击查看!'
   }
 ]
 </script>
@@ -26,12 +39,4 @@ const testData: ColumnProps[] = [
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,20 +1,26 @@
 <template>
-  <ul>
-    <li v-for="column in list" :key="column.id">
-      <img :src="column.avatar" :alt="column.title">
-      <h5>{{ column.title }}</h5>
-      <p>{{ column.description }}</p>
-      <a href="#">进入专栏</a>
-    </li>
-  </ul>
+  <div class="row">
+    <div class="col-4" v-for="column in columnList" :key="column.id">
+      <div class="card h-100 shadow-sm mb-4">
+        <div class="card-body text-center">
+          <img class="round-circle border border-light w-25 my-3" :src="column.avatar" :alt="column.title">
+          <h5 class="card-title">{{ column.title }}</h5>
+          <p class="card-text" style="text-align: left;">{{ column.description }}</p>
+          <a class="btn btn-outline-primary" href="#">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, PropType, computed } from 'vue'
+import Default from '../assets/default.jpeg'
+
 export interface ColumnProps {
   id: number;
   title: string;
-  avatar: string;
+  avatar?: string;
   description: string;
 }
 export default defineComponent({
@@ -24,6 +30,18 @@ export default defineComponent({
       type: Array as PropType<ColumnProps[]>,
       required: true
     }
+  },
+  setup(props) {
+    const columnList = computed(() => (
+      props.list.map(col => {
+        if (!col.avatar) {
+          col.avatar = Default
+        }
+        return col
+      })
+    ))
+
+    return {columnList}
   }
 })
 </script>
